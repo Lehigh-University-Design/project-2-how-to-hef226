d5c9b4302d1be620c122b1de7f2428fc02558cbd
hef226 <hef226@lehigh.edu>
2025-12-01 09:10:31 -0500

    story updates

--- js/script.js
+++ js/script.js
@@ -14,3 +14,136 @@ const observer = new IntersectionObserver(
 );
 
 steps.forEach((step) => observer.observe(step));
+
+
+
+
+
+const slides = [
+  {
+    image: "../improv/Improv1.png",
+  },
+
+  {
+    image: "../improv/Improv2.png",
+  },
+
+  {
+    image: "../improv/Improv3.png",
+  },
+
+  {
+    image: "../improv/Improv3.png",
+  },
+
+  {
+    image: "../improv/Improv4.png",
+  },
+
+  {
+    image: "../improv/Improv5.png",
+  },
+
+  {
+    image: "../improv/Improv6.png",
+  },
+
+  {
+    image: "../improv/Improv7.png",
+  },
+
+  {
+    image: "../improv/Improv8.png",
+  },
+
+  {
+    image: "../improv/Improv9.png",
+  },
+
+  {
+    image: "../improv/Improv10.png",
+  },
+
+  {
+    image: "../improv/Improv11.png",
+  },
+
+  {
+    image: "../improv/Improv12.png",
+  },
+
+  {
+    image: "../improv/Improv13.png",
+  },
+
+  {
+    image: "../improv/Improv14.png",
+  },
+
+  {
+    image: "../improv/Improv15.png",
+  },
+
+  {
+    image: "../improv/Improv16.png",
+  },
+
+  {
+    image: "../improv/Improv17.png",
+  },
+
+  {
+    image: "../improv/Improv18.png",
+  },
+
+  {
+    image: "../improv/Improv19.png",
+  },
+
+  {
+    image: "../improv/Improv20.png",
+  },
+
+  {
+    image: "../improv/Improv21.png",
+  },
+
+  {
+    image: "../improv/Improv22.png",
+  },
+
+  {
+    image: "../improv/Improv23.png",
+  },
+
+  {
+    image: "../improv/Improv24.png",
+  },
+
+  {
+    image: "../improv/Improv25.png",
+  },
+
+  {
+    image: "../improv/Improv26.png",
+  },
+];
+
+let currentSlide = 0;
+
+const bgModule = document.getElementById("bgModule");
+const bgText = document.getElementById("bgText");
+
+bgModule.style.backgroundImage = `url(${slides[0].image})`;
+
+bgModule.addEventListener("click", () => {
+  currentSlide = (currentSlide + 1) % slides.length;
+
+  bgModule.style.backgroundImage = `url(${slides[currentSlide].image})`;
+  bgText.style.opacity = 0;
+
+  setTimeout(() => {
+    bgText.textContent = slides[currentSlide].text;
+    bgText.style.opacity = 1;
+  }, 200);
+});
