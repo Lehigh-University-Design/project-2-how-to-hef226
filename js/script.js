1cbed8e6e61592f71295534c5c162eca55d776ce
hef226 <hef226@lehigh.edu>
2025-12-03 10:10:06 -0500

    Updates

--- js/script.js
+++ js/script.js
@@ -16,113 +16,33 @@ const observer = new IntersectionObserver(
 steps.forEach((step) => observer.observe(step));
 
 const slides = [
-  {
-    image: "../improv/Improv1.png",
-  },
-
-  {
-    image: "../improv/Improv2.png",
-  },
-
-  {
-    image: "../improv/Improv3.png",
-  },
-
-  {
-    image: "../improv/Improv3.png",
-  },
-
-  {
-    image: "../improv/Improv4.png",
-  },
-
-  {
-    image: "../improv/Improv5.png",
-  },
-
-  {
-    image: "../improv/Improv6.png",
-  },
-
-  {
-    image: "../improv/Improv7.png",
-  },
-
-  {
-    image: "../improv/Improv8.png",
-  },
-
-  {
-    image: "../improv/Improv9.png",
-  },
-
-  {
-    image: "../improv/Improv10.png",
-  },
-
-  {
-    image: "../improv/Improv11.png",
-  },
-
-  {
-    image: "../improv/Improv12.png",
-  },
-
-  {
-    image: "../improv/Improv13.png",
-  },
-
-  {
-    image: "../improv/Improv14.png",
-  },
-
-  {
-    image: "../improv/Improv15.png",
-  },
-
-  {
-    image: "../improv/Improv16.png",
-  },
-
-  {
-    image: "../improv/Improv17.png",
-  },
-
-  {
-    image: "../improv/Improv18.png",
-  },
-
-  {
-    image: "../improv/Improv19.png",
-  },
-
-  {
-    image: "../improv/Improv20.png",
-  },
-
-  {
-    image: "../improv/Improv21.png",
-  },
-
-  {
-    image: "../improv/Improv22.png",
-  },
-
-  {
-    image: "../improv/Improv23.png",
-  },
-
-  {
-    image: "../improv/Improv24.png",
-  },
-
-  {
-    image: "../improv/Improv25.png",
-  },
-
-  {
-    image: "../improv/Improv26.png",
-  },
+  { image: "../improv/Improv1.png" },
+  { image: "../improv/Improv2.png" },
+  { image: "../improv/Improv3.png" },
+  { image: "../improv/Improv3.png" },
+  { image: "../improv/Improv4.png" },
+  { image: "../improv/Improv5.png" },
+  { image: "../improv/Improv6.png" },
+  { image: "../improv/Improv7.png" },
+  { image: "../improv/Improv8.png" },
+  { image: "../improv/Improv9.png" },
+  { image: "../improv/Improv10.png" },
+  { image: "../improv/Improv11.png" },
+  { image: "../improv/Improv12.png" },
+  { image: "../improv/Improv13.png" },
+  { image: "../improv/Improv14.png" },
+  { image: "../improv/Improv15.png" },
+  { image: "../improv/Improv16.png" },
+  { image: "../improv/Improv17.png" },
+  { image: "../improv/Improv18.png" },
+  { image: "../improv/Improv19.png" },
+  { image: "../improv/Improv20.png" },
+  { image: "../improv/Improv21.png" },
+  { image: "../improv/Improv22.png" },
+  { image: "../improv/Improv23.png" },
+  { image: "../improv/Improv24.png" },
+  { image: "../improv/Improv25.png" },
+  { image: "../improv/Improv26.png" },
 ];
 
 let currentSlide = 0;
@@ -130,19 +50,22 @@ let currentSlide = 0;
 const bgModule = document.getElementById("bgModule");
 const bgText = document.getElementById("bgText");
 
-bgModule.style.backgroundImage = `url(${slides[0].image})`;
-
-bgModule.addEventListener("click", () => {
-  currentSlide = (currentSlide + 1) % slides.length;
-
-  bgModule.style.backgroundImage = `url(${slides[currentSlide].image})`;
-  bgText.style.opacity = 0;
-
-  setTimeout(() => {
-    bgText.textContent = slides[currentSlide].text;
-    bgText.style.opacity = 1;
-  }, 200);
+// Preload all images in the DOM
+slides.forEach((slide, i) => {
+  const img = document.createElement("img");
+  img.src = slide.image;
+  img.classList.add("slide-img");
+  if (i === 0) img.classList.add("active"); // Show the first image immediately
+  bgModule.appendChild(img);
 });
 
-// ...existing code... (nav toggle removed)
+const images = document.querySelectorAll("#bgModule .slide-img");
 
+bgModule.addEventListener("click", () => {
+  images[currentSlide].classList.remove("active");
+  currentSlide = (currentSlide + 1) % images.length;
+  images[currentSlide].classList.add("active");
+
+  // If you want text changes to be instant too:
+  bgText.textContent = slides[currentSlide].text || "";
+});
